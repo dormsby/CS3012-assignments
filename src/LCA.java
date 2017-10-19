71575b481f31c4e23add0243e9bb015d8e412128
dormsby <dormsby@tcd.ie>
2017-10-19 20:17:03 +0100

    Added Node class

--- src/LCA.java
+++ src/LCA.java
@@ -1,3 +1,17 @@
 
-public class LCA {
+public class LCA<Key extends Comparable<Key>, Value>{
+    private Node root;
+
+    private class Node {
+        private Key key;           // sorted by key
+        private Value val;         // associated data
+        private Node left, right;  // left and right subtrees
+        private Node parent;        // parent of node
+
+        public Node(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
 }
