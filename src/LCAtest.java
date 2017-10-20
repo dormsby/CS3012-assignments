e0a5c219ba587e114f48c1c356e549f9b1bb30e0
dormsby <dormsby@tcd.ie>
2017-10-20 17:58:44 +0100

    Added tests for awkward parameters

--- src/LCAtest.java
+++ src/LCAtest.java
@@ -17,12 +17,14 @@ public class LCAtest {
         lca.put(4, 4);
         lca.put(5,5);
         assertEquals("test get node with key 5", (Integer) 5, lca.get(5));
+        assertEquals("test get node with key 5", null, lca.get(7));
     }
 
     @Test
     public void testGetParentKey(){
         LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
         lca.put(7, 7);   //        _7_
+        assertEquals("test get node with key 5", null, lca.getParentKey(7));
         lca.put(8, 8);   //      /     \
         lca.put(3, 3);   //    _3_      8
         lca.put(1, 1);   //  /     \
@@ -38,6 +40,7 @@ public class LCAtest {
     public void testNumberOfAncestors(){
         LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
         lca.put(7, 7);   //        _7_
+        assertEquals("Test the numberOfAncestors of a node",0, lca.numberOfAncestors(7) );
         lca.put(8, 8);   //      /     \
         lca.put(3, 3);   //    _3_      8
         lca.put(1, 1);   //  /     \
@@ -46,7 +49,7 @@ public class LCAtest {
         lca.put(4, 4);   //   2   4
         lca.put(5, 5);   //        \
                         //         5
-        assertEquals("Test the height of a node",4, lca.numberOfAncestors(5) );
+        assertEquals("Test the numberOfAncestors of a node",4, lca.numberOfAncestors(5) );
 
     }
 
@@ -54,6 +57,9 @@ public class LCAtest {
     public void testAncestorsKeys(){
         LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
         lca.put(7, 7);   //        _7_
+        int[] expected1 = {};
+        Assert.assertArrayEquals(expected1, lca.ancestorsKeys(7));
+
         lca.put(8, 8);   //      /     \
         lca.put(3, 3);   //    _3_      8
         lca.put(1, 1);   //  /     \
@@ -62,14 +68,15 @@ public class LCAtest {
         lca.put(4, 4);   //   2   4
         lca.put(5, 5);   //        \
                         //         5
-        int[] expected = {4,6,3,7};
-        Assert.assertArrayEquals(expected, lca.ancestorsKeys(5));
+        int [] expected2 = {4,6,3,7};
+        Assert.assertArrayEquals(expected2, lca.ancestorsKeys(5));
     }
 
 
     @Test
     public void testFindLCA(){
         LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
+        assertEquals("Test for key of LCA when tree is empty", -1, lca.getLCA(2,4));
         lca.put(7, 7);   //        _7_
         lca.put(8, 8);   //      /     \
         lca.put(3, 3);   //    _3_      8
