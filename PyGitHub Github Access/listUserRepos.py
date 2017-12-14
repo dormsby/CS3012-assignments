57ea9bb393ccee8ef93044a855a692e67c2302bb
dormsby <dormsby@tcd.ie>
2017-12-14 22:38:57 +0000

    Python program that outputs a HTML file that displays a bar chart the number of commits by contributors in a repo

--- PyGitHub Github Access/listUserRepos.py
+++ PyGitHub Github Access/listUserRepos.py
@@ -2,36 +2,40 @@ from github import Github
 import getpass
 from nvd3 import discreteBarChart
 from IPython.core.display import display, HTML
-#nvd3.ipynb.initialize_javascript()
+#nvd3.ipynb.initialize_javascript(use_remote=True)
 
 #https://github.com/PyGithub/PyGithub
 
-#print user repo's
+# Open File to write the D3 Graph
+output_file = open('test-nvd3.html', 'w')
+xdata = []
+ydata = []
+
+chart = discreteBarChart(name='discreteBarChart', height=400, width=400)
 
 
 # using username and password
 user = raw_input("Enter username: ")
 password = getpass.getpass('Enter password: ')
+r = raw_input("Enter repository name: ")
 
 g = Github(user, password)
 
 # Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print repo.name
+repo = g.get_user().get_repo(r)
+i =0
+for con in repo.get_contributors():
+	xdata.insert(i,con.login)
+	ydata.insert(i,con.contributions)
+	i += 1
 
-from nvd3 import pieChart
-
-# Open File to write the D3 Graph
-output_file = open('test-nvd3.html', 'w')
-
-chart = discreteBarChart(name='discreteBarChart', height=400, width=400)
-
-xdata = ["A", "B", "C", "D", "E", "F"]
-ydata = [3, 4, 0, -3, 5, 7]
 
 chart.add_serie(y=ydata, x=xdata)
 chart.buildhtml()
 output_file.write(chart.htmlcontent)
+#display(HTML(chart.htmlcontent))
 
 # close Html file
 output_file.close()
+
+print "HTML chart has been output."
