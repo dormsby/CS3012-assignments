f673c73b58f7252507bdb3780c431eb6e1d5230e
dormsby <dormsby@tcd.ie>
2017-12-14 20:20:17 +0000

    Added function that outputs a html file with a template bar chart using python-nvd3

--- PyGitHub Github Access/listUserRepos.py
+++ PyGitHub Github Access/listUserRepos.py
@@ -1,6 +1,8 @@
 from github import Github 
 import getpass
-
+from nvd3 import discreteBarChart
+from IPython.core.display import display, HTML
+#nvd3.ipynb.initialize_javascript()
 
 #https://github.com/PyGithub/PyGithub
 
@@ -17,4 +19,19 @@ g = Github(user, password)
 for repo in g.get_user().get_repos():
     print repo.name
 
+from nvd3 import pieChart
 
+# Open File to write the D3 Graph
+output_file = open('test-nvd3.html', 'w')
+
+chart = discreteBarChart(name='discreteBarChart', height=400, width=400)
+
+xdata = ["A", "B", "C", "D", "E", "F"]
+ydata = [3, 4, 0, -3, 5, 7]
+
+chart.add_serie(y=ydata, x=xdata)
+chart.buildhtml()
+output_file.write(chart.htmlcontent)
+
+# close Html file
+output_file.close()
